2405666d6c6af7107c71519a65db32a1b007e1e2
Gonpeche <espeche.g@gmail.com>
2020-02-18 10:53:52 -0300

    modify readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+    @function setup
+    @param {object} props - Component props specific to this setup
+    @param {object} state - Initial state for setup
+*/
+
+const setup = ( props = {}, state = null ) => {
+    const wrapper = shallow(<App {...props} />)
+    if (state) wrapper.setState(state);
+    return wrapper;
+}
 
 test('renders correctly', () => {
 
